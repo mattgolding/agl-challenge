b85102222e17e2a6333347c14cbf55fec17ad5f2
Matt Golding <mp.golding@gmail.com>
2017-06-27 20:08:14 +1000

    getting the data remotley

--- readme.md
+++ readme.md
@@ -12,10 +12,10 @@ The following command will compile the JS Modules
 
 * npm run webserver
 
-The application can then be seen on http://localhost:8080
+The application can then be seen on [http://localhost:8080](http://localhost:8080)
 
 ## Unit test
 
 Unit tests were writen with mocha and chai.
 
-They can be viewed in the browser by browsing to http://localhost:8080/testRunner.html
+They can be viewed in the browser by browsing to [http://localhost:8080/testRunner.html](http://localhost:8080/testRunner.html)
--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
     // get the info from ajax server
-    var json = [{"name":"Bob","gender":"Male","age":23,"pets":[{"name":"Garfield","type":"Cat"},{"name":"Fido","type":"Dog"}]},{"name":"Jennifer","gender":"Female","age":18,"pets":[{"name":"Garfield","type":"Cat"}]},{"name":"Steve","gender":"Male","age":45,"pets":null},{"name":"Fred","gender":"Male","age":40,"pets":[{"name":"Tom","type":"Cat"},{"name":"Max","type":"Cat"},{"name":"Sam","type":"Dog"},{"name":"Jim","type":"Cat"}]},{"name":"Samantha","gender":"Female","age":40,"pets":[{"name":"Tabby","type":"Cat"}]},{"name":"Alice","gender":"Female","age":64,"pets":[{"name":"Simba","type":"Cat"},{"name":"Nemo","type":"Fish"}]}];
+    $.ajax({
+        url: 'http://agl-developer-test.azurewebsites.net/people.json',
+        dataType: 'jsonp', // jsonp for cross site domain
+        success: onLoadDataSuccess
+    });
+});
 
-    // populate the objects
+function onLoadDataSuccess(data) {
+    // populate the people objects
     var people = [];
-    json.forEach(function (personData) {
+    data.forEach(function (personData) {
         var person = new Person(personData);
 
         people.push(person);
     });
 
     // get two lists one for female and one for male
-    var males = getPeopleByGender(people, 'Male');
-    var females = getPeopleByGender(people, 'Female');
+    var males = getPeopleByGender(people, 'male');
+    var females = getPeopleByGender(people, 'female');
 
     // call the helper function as we do duplicate things inside for male and female
     writeOutGenderPetNames(males, 'cat', '.male-owned-pets');
     writeOutGenderPetNames(females, 'cat', '.female-owned-pets');
-});
+}
 
 // Helper function for handling duplicate code
 function writeOutGenderPetNames (peopleList, animalType, domSelector) {
